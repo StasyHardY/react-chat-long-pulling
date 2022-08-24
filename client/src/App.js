9efd168778880ac79e5b41bfacfd38b7f964e663
Max <mklevogin@mail.ru>
2022-08-24 14:59:21 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css'
+import { LongPulling } from './LongPulling';
+
+
+function App() {
+
+  return (
+      <div>
+        <LongPulling/>
+      </div>
+  )
+}
+
+
+export default App;
